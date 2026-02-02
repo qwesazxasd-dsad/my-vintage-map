2b9b93491aa0de116216dc3753a809811fbcdd66
qwesazxasd-dsad <hwa2367@gmail.com>
2026-02-02 21:11:43 +0900

    replace provider with script tag

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,10 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { NavermapsProvider } from 'react-naver-maps';
+// import { NavermapsProvider } from 'react-naver-maps';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-    <NavermapsProvider>
+    <React.StrictMode>
       <App />
-    </NavermapsProvider>
+    </React.StrictMode>
 )
